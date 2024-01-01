a9907e552864c0956997d5b8d904c74f3a0f7822
Quoc Lien <lien.h.quoc@gmail.com>
2024-01-01 21:40:16 +0700

    feat: Add Swagger, follow the kebab-case mongoDb naming convention

--- backend/Program.cs
+++ backend/Program.cs
@@ -3,17 +3,21 @@ using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var databaseName = "task_hub";
 var mongoConnectionString = builder.Configuration.GetConnectionString("MongoDBConnection");
 var mongoClient = new MongoClient(mongoConnectionString);
-var mongoDatabase = mongoClient.GetDatabase("TaskHub");
+var mongoDatabase = mongoClient.GetDatabase(databaseName);
 builder.Services.AddSingleton(mongoDatabase);
-builder.Services.AddDbContext<TaskDbContext>(options => options.UseMongoDB(mongoClient, "TaskHub")); // Add the TaskDbContext as a service
+builder.Services.AddDbContext<TaskDbContext>(options => options.UseMongoDB(mongoClient, databaseName)); // Add the TaskDbContext as a service
 builder.Services.AddScoped<ITaskRepository, TaskRepository>();
 builder.Services.AddScoped<ITaskService, TaskService>();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddControllers(); // Register your controllers
+builder.Services.AddControllers();
+
+//builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -23,6 +27,11 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
